2b1fa940dea42eab84f322336e1c9e3dbddcb004
Randy Eckenrode <randy@largeandhighquality.com>
2021-12-06 12:07:32 -0500

    day 6: `tick` in place

--- rust/src/days/day6/fish.rs
+++ rust/src/days/day6/fish.rs
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
-use std::{io::BufRead, mem::swap};
+use std::io::BufRead;
 
 use anyhow::Result;
 use itertools::Itertools;
 
 const DEFAULT_TIMER: usize = 6;
 const NEW_FISH_DELAY: usize = 2;
-const ARRAY_SIZE: usize = DEFAULT_TIMER + NEW_FISH_DELAY + 2;
+const ARRAY_SIZE: usize = DEFAULT_TIMER + NEW_FISH_DELAY + 1;
 
 #[derive(Debug, PartialEq)]
 pub(crate) struct Fish {
@@ -40,16 +40,13 @@ impl Fish {
     }
 
     pub(crate) fn tick(&mut self) {
-        let mut buf = [0; ARRAY_SIZE];
-        for (idx, count) in self.buf.iter().enumerate() {
-            if idx == 0 {
-                buf[DEFAULT_TIMER] = *count;
-                buf[DEFAULT_TIMER + NEW_FISH_DELAY] = *count;
-            } else {
-                buf[idx - 1] += count;
-            }
+        self.buf[DEFAULT_TIMER + 1] += self.buf[0];
+        let birthed_fish = self.buf[0];
+        let mut it = self.buf.iter_mut().peekable();
+        while let (Some(elem), Some(next)) = (it.next(), it.peek()) {
+            *elem = **next;
         }
-        swap(&mut buf, &mut self.buf);
+        self.buf[DEFAULT_TIMER + NEW_FISH_DELAY] = birthed_fish;
     }
 }
 
@@ -59,7 +56,7 @@ mod tests {
 
     #[test]
     fn read_input_parses_the_fish() -> Result<()> {
-        let expected_result = Fish::new([0, 1, 1, 2, 1, 0, 0, 0, 0, 0]);
+        let expected_result = Fish::new([0, 1, 1, 2, 1, 0, 0, 0, 0]);
         let result = Fish::parse("3,4,3,1,2".as_bytes())?;
         assert_eq!(result, expected_result);
         Ok(())
@@ -67,8 +64,8 @@ mod tests {
 
     #[test]
     fn tick_decreases_the_timer_of_the_fish_by_one() {
-        let expected_fish = Fish::new([0, 0, 0, 0, 1, 0, 0, 0, 0, 0]);
-        let mut fish = Fish::new([0, 0, 0, 0, 0, 1, 0, 0, 0, 0]);
+        let expected_fish = Fish::new([0, 0, 0, 0, 1, 0, 0, 0, 0]);
+        let mut fish = Fish::new([0, 0, 0, 0, 0, 1, 0, 0, 0]);
         fish.tick();
         assert_eq!(fish, expected_fish);
     }
@@ -76,23 +73,23 @@ mod tests {
     #[test]
     fn tick_at_zero_resets_the_timer_of_the_fish_to_the_limit() {
         let expected_fish = 1;
-        let mut fish = Fish::new([1, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+        let mut fish = Fish::new([1, 0, 0, 0, 0, 0, 0, 0, 0]);
         fish.tick();
         assert_eq!(fish.buf[DEFAULT_TIMER], expected_fish);
     }
 
     #[test]
     fn tick_at_zero_produces_an_additional_fish() {
-        let expected_fish = Fish::new([0, 0, 0, 0, 0, 0, 1, 0, 1, 0]);
-        let mut fish = Fish::new([1, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+        let expected_fish = Fish::new([0, 0, 0, 0, 0, 0, 1, 0, 1]);
+        let mut fish = Fish::new([1, 0, 0, 0, 0, 0, 0, 0, 0]);
         fish.tick();
         assert_eq!(fish, expected_fish);
     }
 
     #[test]
     fn tick_new_fish_does_not_produce_another_fish() {
-        let expected_fish = Fish::new([0, 0, 0, 0, 0, 1, 0, 1, 0, 0]);
-        let mut fish = Fish::new([0, 0, 0, 0, 0, 0, 1, 0, 1, 0]);
+        let expected_fish = Fish::new([0, 0, 0, 0, 0, 1, 0, 1, 0]);
+        let mut fish = Fish::new([0, 0, 0, 0, 0, 0, 1, 0, 1]);
         fish.tick();
         assert_eq!(fish, expected_fish);
     }
@@ -100,7 +97,7 @@ mod tests {
     #[test]
     fn count_returns_total_number_of_fish() {
         let expected_count = 88;
-        let fish = Fish::new([1, 1, 2, 3, 5, 8, 13, 21, 34, 0]);
+        let fish = Fish::new([1, 1, 2, 3, 5, 8, 13, 21, 34]);
         let count = fish.count();
         assert_eq!(count, expected_count);
     }
