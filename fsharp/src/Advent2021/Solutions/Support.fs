9af4b368f26c90600fbbd64e8426a8fd2007ba11
Randy Eckenrode <randy@largeandhighquality.com>
2021-12-19 09:17:19 -0500

    Add `lines` helper function

--- fsharp/src/Advent2021/Solutions/Support.fs
+++ fsharp/src/Advent2021/Solutions/Support.fs
@@ -9,35 +9,42 @@ open System.IO
 open System.Reflection
 open System.Threading.Tasks
 
-open FSharpx.Option
+open FSharp.Control
+open FSharpPlus
 open FSharpx.Text
 
 [<AutoOpen>]
 module Support =
     let register (root: RootCommand) =
         let getDayNumber s =
-            maybe {
+            monad' {
                 let! matchResult = Regex.tryMatch @"[A-Za-z](\d+)" s
                 let! day = matchResult.GroupValues |> List.tryItem 0
-                return! ofBoolAndValue (Int32.TryParse day)
+                return! Option.ofPair (Int32.TryParse day)
             }
             |> Option.defaultValue -1
 
         let getCommand (t: Type) =
-            maybe {
+            monad' {
                 let! props = t.GetProperties () |> Option.ofObj
+
                 let! cmd =
                     props
-                    |> Seq.filter (fun p -> p.Name = "command" && p.PropertyType = typeof<Command>)
+                    |> Seq.filter (fun (p: PropertyInfo) ->
+                        p.Name = "command"
+                        && p.PropertyType = typeof<Command>)
                     |> Seq.tryExactlyOne
+
                 return downcast cmd.GetValue null
             }
 
         let assembly = Assembly.GetExecutingAssembly ()
+
         let commands =
             assembly.GetTypes ()
             |> Array.choose getCommand
             |> Array.sortBy (fun (cmd: Command) -> getDayNumber cmd.Name)
+
         commands |> Array.iter root.AddCommand
         root
 
@@ -48,16 +55,25 @@ module Support =
             console.Error.Write $"Error parsing file: {message}"
             1
 
+    let rec lines (reader: TextReader) =
+        asyncSeq {
+            match! Async.AwaitTask (reader.ReadLineAsync ()) with
+            | null -> ()
+            | line ->
+                yield line
+                yield! lines reader
+        }
+
     module Command =
         let create name description (handler: 'a -> IConsole -> Task<int>) =
-            let cmd = Command (
-                name,
-                description,
-                Handler = CommandHandler.Create handler
-            )
-            cmd.AddOption <| Option<FileInfo> (
+            let cmd =
+                Command (name, description, Handler = CommandHandler.Create handler)
+
+            cmd.AddOption
+            <| Option<FileInfo> (
                 aliases = [| "-i"; "--input" |],
                 description = "the day’s input file",
                 IsRequired = true
             )
+
             cmd
